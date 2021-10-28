25647ce0f3017fdd839c3aae188c5145d497cf4e
unknown <richdanmac@gmail.com>
2021-10-28 17:09:03 -0500

    added advanced section

--- popular-downloader.js
+++ popular-downloader.js
@@ -1,7 +1,22 @@
 const rp = require('request-promise')
 const path = require('path')
+const fs = require('fs')
 
 rp('https://reddit.com/r/popular.json')
     .then((res) => {
-        JSON.parse()
+        JSON.parse(res).data.children.map((article) => {
+            if (path.extname(article.data.url) === '.jpg' || path.extname(article.data.url) === '.png' || path.extname(article.data.url) === '.gif') {
+                rp(article.data.url)
+                    .then((img) => {
+                        let base64Data = img.replace(/^data:image\/png;base64,/, "");
+
+                        fs.writeFile(`./downloads/${article.data.id}${path.extname(article.data.url)}`, base64Data, "base64", (err) => {
+                            if (err) throw err;
+                        });
+                    })
+                    .catch((err) => {
+                        if (err) throw err;
+                    });
+            }
+        })
     })
